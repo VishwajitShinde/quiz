1cf6cda9763c07fd16914868a6869ab0ca82ebb6
dhamma4u <dhamma4u@gmail.com>
2020-07-20 14:10:44 +0530

    Cleared Everything Earlier Which is not working Now Make it working

--- src/main/java/com/api/quiz/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.api.quiz.repository;
-
-import com.api.quiz.entity.UserDao;
-import org.springframework.data.repository.CrudRepository;
-public interface UserRepository extends CrudRepository<UserDao, Integer> {
-    UserDao findByUsername(String username);
-}
